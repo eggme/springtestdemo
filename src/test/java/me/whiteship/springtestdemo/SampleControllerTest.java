ffcd3cbde916785006a094136dca45278afe4f83
kyyeto9984 <kyyeto9984@naver.com>
2020-09-08 18:33:36 +0900

    Initial commit

---
+++ src/test/java/me/whiteship/springtestdemo/SampleControllerTest.java
@@ -0,0 +1,8 @@
+package me.whiteship.springtestdemo;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class SampleControllerTest {
+}
---
+++ src/test/java/me/whiteship/springtestdemo/sample/SpringtestdemoApplicationTests.java
@@ -0,0 +1,44 @@
+package me.whiteship.springtestdemo.sample;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class SpringtestdemoApplicationTests {
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @MockBean
+    SampleService mocksSampleService;
+
+    @Test
+    void contextLoads() throws Exception{
+
+        when(mocksSampleService.getName()).thenReturn("seungjun");
+
+        webTestClient.get().uri("/hello").exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class).isEqualTo("hello seungjun");
+
+        String forObject = testRestTemplate.getForObject("/hello", String.class);
+        assertThat(forObject).isEqualTo("hello seungjun");
+    }
+
+}
